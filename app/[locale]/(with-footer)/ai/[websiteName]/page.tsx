2da50e4c45673746cbba1f28ccd6a50d6477d317
tianbao <mtian999@163.com>
2024-06-15 10:25:27 +0800

    feat:SEO优化

--- app/[locale]/(with-footer)/ai/[websiteName]/page.tsx
+++ app/[locale]/(with-footer)/ai/[websiteName]/page.tsx
@@ -18,20 +18,30 @@ export async function generateMetadata({
   });
   const res = await getWebNavigationDetail(websiteName);
 
+  const DetailPageKeyword = await getTranslations('DetailPageKeyword');
+  let currentDetailPageKeyword = DetailPageKeyword(res.data?.name);
+  if (currentDetailPageKeyword === `DetailPageKeyword.${res.data?.name}`) {
+    currentDetailPageKeyword = res.data.name || '';
+  }
   return {
     title: `${res.data.title} | ${t('titleSubfix')}`,
     description: res.data.content,
-    keywords: res.data.name,
+    keywords: currentDetailPageKeyword,
   };
 }
 
 export default async function Page({ params: { websiteName } }: { params: { websiteName: string } }) {
-  const t = await getTranslations('Startup.detail');
   const res = await getWebNavigationDetail(websiteName);
   const { data } = res;
 
   if (!data) return null;
 
+  const t = await getTranslations('Startup.detail');
+  const AIDetail = await getTranslations('AIDetail');
+  let currentPageAIDetail = AIDetail(data?.name);
+  if (currentPageAIDetail === `AIDetail.${data?.name}`) {
+    currentPageAIDetail = data?.detail || '';
+  }
   return (
     <div className='w-full'>
       <div className='flex flex-col px-6 py-5 lg:h-[323px] lg:flex-row lg:justify-between lg:px-0 lg:py-10'>
@@ -72,7 +82,7 @@ export default async function Page({ params: { websiteName } }: { params: { webs
       <Separator className='bg-[#010101]' />
       <div className='mb-5 px-3 lg:px-0'>
         <h2 className='my-5 text-2xl text-white/40 lg:my-10'>{t('introduction')}</h2>
-        <MarkdownProse markdown={data?.detail || ''} />
+        <MarkdownProse markdown={currentPageAIDetail} />
       </div>
     </div>
   );
